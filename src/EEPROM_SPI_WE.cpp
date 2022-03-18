7231d9805eb26d0606616a67b97e185e2bcc8b3a
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-03-18 12:27:41 +0100

    Add files via upload

---
+++ src/EEPROM_SPI_WE.cpp
@@ -0,0 +1,246 @@
+/********************************************************************
+* This is a library for SPI EEPROMs like the 25LCxxx, 25Axxx, etc.
+*
+* In case you like it would be cool if you give it a star.
+* 
+* If you find bugs, please inform me!
+* 
+* Written by Wolfgang (Wolle) Ewald
+*
+* You will find a tutorial on my website soon:
+* https://wolles-elektronikkiste.de/    (German)
+* https://wolles-elektronikkiste.de/en/ (English)
+*
+*********************************************************************/
+
+#include "EEPROM_SPI_WE.h"
+
+/************  Constructors ************/
+
+EEPROM_SPI_WE::EEPROM_SPI_WE(SPIClass *s, int cs, int wp){
+    _spi = s;
+    csPin = cs;
+	wpPin = wp;
+}
+
+EEPROM_SPI_WE::EEPROM_SPI_WE(int cs, int wp){
+    _spi = &SPI;
+    csPin = cs;
+	wpPin = wp;
+}
+
+/************ Basic settings ************/
+    
+bool EEPROM_SPI_WE::init(){    
+    _spi->begin();
+	mySPISettings = SPISettings(8000000, MSBFIRST, SPI_MODE0);
+    pinMode(csPin, OUTPUT);
+    digitalWrite(csPin, HIGH);
+	
+	if(wpPin != 999){
+		pinMode(wpPin,OUTPUT);
+		digitalWrite(wpPin, HIGH);
+	}
+	
+    pageSize = 16;
+	writeProtect(PROTECT_NONE);
+	eepromWriteStatusReg(EEP_WREN);
+	if(eepromReadStatusReg() & 0x02){
+		eepromWriteStatusReg(EEP_WRDI);
+		return true;
+	}
+	else return false;	
+}
+
+void EEPROM_SPI_WE::setPageSize(eeprom_pageSize eps){
+    pageSize = 16 << eps;
+}
+
+void EEPROM_SPI_WE::deepPowerDown(){
+	eepromWriteStatusReg(EEP_DPD);
+}
+
+uint8_t EEPROM_SPI_WE::powerUpAndReadID(){
+	uint8_t id = 0;
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_RDID); 
+	_spi->transfer(0xFF); // dummy address
+	_spi->transfer(0xFF);
+	id = _spi->transfer(0x00);
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	while(isBusy()){}
+	return id;
+}
+
+/************ read and write ************/
+
+void EEPROM_SPI_WE::erasePage(uint32_t addr){
+	eepromWriteEnable();
+	
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_PE);
+	_spi->transfer((uint8_t)(addr>>8));
+	_spi->transfer((uint8_t)(addr&0xFF));
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	
+	while(isBusy()){}	
+}
+
+void EEPROM_SPI_WE::eraseSector(uint32_t addr){
+	eepromWriteEnable();
+	
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_SE);
+	_spi->transfer((uint8_t)(addr>>8));
+	_spi->transfer((uint8_t)(addr&0xFF));
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	
+	while(isBusy()){}
+}
+
+void EEPROM_SPI_WE::eraseCompleteEEPROM(){
+	eepromWriteEnable();
+	
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_CE);
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	
+	while(isBusy()){}	
+}
+
+void EEPROM_SPI_WE::write(uint32_t addr, uint8_t val){
+	if (read(addr) != val) 
+		writeEEPROM(addr, &val, 1);
+	while(isBusy()){}
+}
+
+uint8_t EEPROM_SPI_WE::read(uint32_t addr){
+	uint8_t byteVal;
+	readEEPROM(addr, &byteVal, 1);
+	return byteVal;
+}
+
+void EEPROM_SPI_WE::writeProtect(eeprom_writeProtect sectors){
+	eepromWriteEnable();
+	
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_WRSR);
+	_spi->transfer((sectors << 2) | 0x80);
+	digitalWrite(csPin, HIGH); 
+	_spi->endTransaction();
+	while(isBusy()){}
+}
+
+void EEPROM_SPI_WE::protectStatusRegister(bool protect){
+	uint8_t statusReg = eepromReadStatusReg();
+	if(!protect && (wpPin != 999)){
+		digitalWrite(wpPin, HIGH);
+	}
+	else if(protect && (wpPin != 999)){
+		statusReg |= 0x80;
+		eepromWriteEnable();
+		eepromWriteStatusReg(statusReg);
+		digitalWrite(wpPin, LOW);
+	}
+}	
+
+uint8_t EEPROM_SPI_WE::eepromReadStatusReg(){
+	uint8_t statusReg = 0;
+	
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_RDSR); 
+	statusReg = _spi->transfer(0x00); 
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	
+	return statusReg;
+}
+
+bool EEPROM_SPI_WE::isBusy(){
+	delayMicroseconds(50);
+	uint8_t statusReg = eepromReadStatusReg();
+	return (statusReg & 0x01);
+}
+
+void EEPROM_SPI_WE::setSPIClockSpeed(unsigned long clock){
+	mySPISettings = SPISettings(clock, MSBFIRST, SPI_MODE0);
+}
+
+/************************************************ 
+     Private Functions
+*************************************************/
+
+void EEPROM_SPI_WE::eepromWriteEnable(){
+	eepromWriteStatusReg(EEP_WREN);	
+}
+
+void EEPROM_SPI_WE::eepromWriteStatusReg(uint8_t cmd){
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(cmd); 
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+	
+	while(isBusy()){}
+}
+
+void EEPROM_SPI_WE::writeEEPROM(uint32_t addr, const uint8_t *buf, uint16_t sizeOfBuf){
+	uint16_t noOfBytesStillToWrite = sizeOfBuf;
+	uint16_t arrayIndex = 0;
+	
+	while((noOfBytesStillToWrite != 0)){
+		uint16_t chunk = noOfBytesStillToWrite;
+		uint16_t positionInPage = addr % pageSize;
+		uint16_t spaceLeftInPage = pageSize - positionInPage;
+		
+		if(spaceLeftInPage < noOfBytesStillToWrite){
+			chunk = spaceLeftInPage;
+		}
+				
+		eepromWriteEnable();
+		
+		_spi->beginTransaction(mySPISettings);
+		digitalWrite(csPin, LOW);
+		_spi->transfer(EEP_WRITE);
+		_spi->transfer((uint8_t)(addr>>8));
+		_spi->transfer((uint8_t)(addr&0xFF));
+		
+		for(uint16_t i=arrayIndex; i<(arrayIndex + chunk); i++){
+			_spi->transfer((uint8_t)buf[i]);
+		}
+		
+		digitalWrite(csPin, HIGH);
+		_spi->endTransaction();
+		
+		addr += chunk;
+		arrayIndex += chunk;
+		noOfBytesStillToWrite -= chunk;
+		while(isBusy()){}		
+	}
+}
+
+void EEPROM_SPI_WE::readEEPROM(uint32_t addr, uint8_t *buf, uint16_t sizeOfBuf){    
+	_spi->beginTransaction(mySPISettings);
+	digitalWrite(csPin, LOW);
+	_spi->transfer(EEP_READ); 
+	_spi->transfer((uint8_t)(addr>>8));
+	_spi->transfer((uint8_t)(addr&0xFF));
+	for(uint16_t i=0; i<sizeOfBuf; i++){
+		buf[i] = _spi->transfer(0x00);
+	}
+	digitalWrite(csPin, HIGH);
+	_spi->endTransaction();
+}
+
+
+
--- src/EEPROM_SPI_WE.h
+++ src/EEPROM_SPI_WE.h
@@ -1,1 +1,99 @@
+/********************************************************************
+* This is a library for SPI EEPROMs like the 25LCxxx, 25Axxx, etc.
+*
+* In case you like it would be cool if you give it a star.
+* 
+* If you find bugs, please inform me!
+* 
+* Written by Wolfgang (Wolle) Ewald
+*
+* You will find a tutorial on my website soon:
+* https://wolles-elektronikkiste.de/    (German)
+* https://wolles-elektronikkiste.de/en/ (English)
+*
+*********************************************************************/
+
+#ifndef EEPROM_SPI_WE_H_
+#define EEPROM_SPI_WE_H_
+
+#if ARDUINO < 100
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+
+#include <SPI.h>
+#define EEP_READ	0x03  // write
+#define EEP_WRITE 	0x02  // read
+#define EEP_WREN 	0x06  // write enable
+#define EEP_WRDI 	0x04  // write disable
+#define EEP_RDSR 	0x05  // read status register
+#define EEP_WRSR	0x01  // write status register
+#define EEP_PE 		0x42  // page erase
+#define EEP_SE 		0xD8  // sector erase
+#define EEP_CE 		0xC7  // chip erase
+#define EEP_RDID 	0xAB  // release from deep power down and read electronic signature
+#define EEP_DPD 	0xB9  // deep power down mode
+
+
+typedef enum EEPROM_PAGE_SIZE {
+    EEPROM_PAGE_SIZE_16, EEPROM_PAGE_SIZE_32, EEPROM_PAGE_SIZE_64, EEPROM_PAGE_SIZE_128, EEPROM_PAGE_SIZE_256
+} eeprom_pageSize;
+
+typedef enum EEPROM_WRITE_PROTECT {
+    PROTECT_NONE, PROTECT_UPPER_QUARTER, PROTECT_UPPER_HALF, PROTECT_ALL
+} eeprom_writeProtect;
+
+class EEPROM_SPI_WE
+{
+public: 
+    
+    EEPROM_SPI_WE(SPIClass *s, int cs, int wp = 999);
+    EEPROM_SPI_WE(int cs, int wp = 999);
+        
+    bool init();
+	void setPageSize(eeprom_pageSize eps);
+	void deepPowerDown();
+	uint8_t powerUpAndReadID();
+	void erasePage(uint32_t addr);
+	void eraseSector(uint32_t addr);
+	void eraseCompleteEEPROM();
+	void write(uint32_t addr, uint8_t val);
+	uint8_t read(uint32_t addr);
+	void writeProtect(eeprom_writeProtect sectors);
+	void protectStatusRegister(bool protect = true);
+	
+	template <typename T>
+	T &get(uint32_t addr, T &t){
+		uint8_t *data = (uint8_t *)&t;
+		readEEPROM(addr, data, sizeof(T)); 
+		return t;
+	}
+
+	template <typename T>
+	const T &put(uint32_t addr, const T &t){ 
+		const uint8_t *data = (const uint8_t *)&t;
+		writeEEPROM(addr, data, sizeof(T)); 
+		return t;
+	}
+	
+	bool isBusy();
+	uint8_t eepromReadStatusReg(); 
+	void setSPIClockSpeed(unsigned long clock);	
+        
+private:
+    SPIClass *_spi;
+	SPISettings mySPISettings;
+    int16_t csPin;
+	int16_t wpPin;
+	uint16_t pageSize;
+		
+	void eepromWriteEnable();
+	void eepromWriteStatusReg(uint8_t cmd);
+	void writeEEPROM(uint32_t addr, const uint8_t *buf, uint16_t sizeOfBuf);
+	void readEEPROM(uint32_t addr, uint8_t *buf, uint16_t sizeOfBuf);	
+};
+
+#endif
+
 
