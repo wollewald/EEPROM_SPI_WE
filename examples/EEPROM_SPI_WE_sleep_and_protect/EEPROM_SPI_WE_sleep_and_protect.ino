e63a45335faf01686854f7d603b45d1eef184438
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2022-05-29 12:52:30 +0200

    Update EEPROM_SPI_WE_sleep_and_protect.ino

--- examples/EEPROM_SPI_WE_sleep_and_protect/EEPROM_SPI_WE_sleep_and_protect.ino
+++ examples/EEPROM_SPI_WE_sleep_and_protect/EEPROM_SPI_WE_sleep_and_protect.ino
@@ -1,4 +1,4 @@
-/********************************************************************
+/******************************************************************************
 * This is an example sketch for the EEPROM_SPI_WE library.
 *
 * It shows how to apply the deep power down mode and different 
@@ -6,11 +6,11 @@
 *  
 * Written by Wolfgang (Wolle) Ewald.
 *
-* You will find a tutorial on my website soon:
-* https://wolles-elektronikkiste.de/    (German)
-* https://wolles-elektronikkiste.de/en/ (English)
+* Here you find the detailed documentation:
+* https://wolles-elektronikkiste.de/en/eeprom-part-3-external-spi-eeproms   (English)
+* https://wolles-elektronikkiste.de/eeprom-teil-3-externe-spi-eeproms       (German)
 *
-*********************************************************************/
+*******************************************************************************/
 
 #include <SPI.h>
 #include <EEPROM_SPI_WE.h>
