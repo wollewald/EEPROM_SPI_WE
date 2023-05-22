4cb9fabd2e51c0e622e31a5e68d618126874a0fc
Wolfgang (Wolle) Ewald <wolfgang.ewald@wolles-elektronikkiste.de>
2023-05-22 15:44:50 +0200

    Add files via upload

--- examples/EEPROM_SPI_WE_small_eeproms/EEPROM_SPI_WE_small_eeproms.ino
+++ examples/EEPROM_SPI_WE_small_eeproms/EEPROM_SPI_WE_small_eeproms.ino
@@ -1,7 +1,9 @@
 /*******************************************************************************
 * This is an example sketch for the EEPROM_SPI_WE library
 *
-* It shows how to use small EEPROMs of 128, 256 or 512 bytes. 
+* It shows how to use small EEPROMs of 128, 256, 512 or 1024 bytes. These types 
+* have typically a designation .....0x0y with x = 1, 2, 4 or 8, e.g. 25LC040A or 
+* ST95020.
 * 
 * Be aware that small EEPROMs might not support all functions. If in doubt, check 
 * the data sheet.  
@@ -41,11 +43,13 @@ void setup(){
     while(1);
   }
 
+  /* You can change the SPI clock speed. The default is 8 MHz. 
+     Check the data sheet which clock speed is allowed. The 
+     frequency limit migh also depend on the voltage. */
+  myEEP.setSPIClockSpeed(2000000); // use AFTER init()! 
+
   /* Function to set small EEPROMs */
   myEEP.setSmallEEPROM();
-  
-  /* You can change the SPI clock speed. The default of is 8 MHz */
-  //myEEP.setSPIClockSpeed(4000000); // use AFTER init()!
   
   /* Select the page size of your EEPROM.
    * Choose EEPROM_PAGE_SIZE_xxx,  
@@ -53,6 +57,17 @@ void setup(){
    */
   myEEP.setPageSize(EEPROM_PAGE_SIZE_16);
 
+  /* Small EEPROMs do not have an embedded instruction to erase
+   * the complete chip. There the eraseCompleteEEPROM function will
+   * override every byte with 0. For this, you need to specify the 
+   * EEPROM size in kbits:
+   * EEPROM_KBITS_x with x = 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 or 2048
+   */
+  // myEEP.setMemorySize(EEPROM_KBITS_2); // adjust and uncomment
+  // Serial.print("Memory size in kbits: ");
+  // Serial.println(myEEP.getMemorySize());
+  // myEEP.eraseCompleteEEPROM();
+  
   byte byteToWrite = 42; 
   myEEP.write(10, byteToWrite);  // write a byte to EEPROM address 10
   byte byteToRead = myEEP.read(10);
@@ -81,9 +96,9 @@ void setup(){
   Serial.println(charArrayToRead);
 
   String stringToWrite = "Hello, I am a test string";
-  unsigned int nextAddr = myEEP.putString(75, stringToWrite);   // String objects need a different put function
+  unsigned int nextAddr = myEEP.putString(105, stringToWrite);   // String objects need a different put function
   String stringToRead = "";
-  myEEP.getString(75, stringToRead);    // String objects need a different get function
+  myEEP.getString(105, stringToRead);    // String objects need a different get function
   Serial.print("String read: ");
   Serial.println(stringToRead);
   Serial.print("Next free address: ");
